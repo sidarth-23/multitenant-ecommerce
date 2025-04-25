1b0662e408e06dd5bff594549cabc0953b7e3e61
Sidarth G <sidarthdev157@gmail.com>
2025-04-25 23:47:57 +0530

    Added subcategory field in the category collection in backend

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,12 @@
-export default async function Home() {
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
 
-  return (
-    <div className="flex flex-col gap-y-4 p-4">
-      
-    </div>
-  );
+export default async function Home() {
+  const payload = await getPayload({ config: configPromise });
+  const data = await payload.find({
+    collection: "categories",
+  });
+  console.log(data);
+
+  return <div className="p-4">{JSON.stringify(data, null, 2)}</div>;
 }
--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,15 +1,39 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from "payload";
 
 export const Categories: CollectionConfig = {
-  slug: 'categories',
+  slug: "categories",
   access: {
     read: () => true,
   },
   fields: [
     {
-      name: 'name',
-      type: 'text',
+      name: "name",
+      type: "text",
       required: true,
     },
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+      unique: true,
+      index: true,
+    },
+    {
+      name: "color",
+      type: "text",
+    },
+    {
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories",
+      hasMany: false,
+    },
+    {
+      name: "subcategories",
+      type: "join",
+      collection: "categories",
+      on: "parent",
+      hasMany: false,
+    },
   ],
-}
+};
