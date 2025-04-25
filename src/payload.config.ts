063c4513dcbbc171406219711cec7ecf7c33efa2
Sidarth G <sidarthdev157@gmail.com>
2025-04-25 23:28:11 +0530

    Fixed type module for node lts to work

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,9 +7,9 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
-import { Categories } from './collections/Categories'
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { Categories } from "./collections/Categories";
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
